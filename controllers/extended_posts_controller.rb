3ee9c19d6e43011b9d64903bd583c76d40053d3b
Daniel Fernández <dfp.fernandez@gmail.com>
2015-08-20 18:59:31 +0100

    Don't show top answer on private messages

--- controllers/extended_posts_controller.rb
+++ controllers/extended_posts_controller.rb
@@ -7,9 +7,13 @@ PostsController.class_eval do
   def highlight_post
     begin
       post = Post.find(params[:post_id])
-      most_liked_post = post.topic.posts.joins(user: [group_users:[:group]]).where(groups: {name: params[:group]}).order(like_count: :desc).limit(1).first
-      if most_liked_post.id == post.id
-        render json: { highlight_post: true }
+      unless post.topic.private_message?
+        most_liked_post = post.topic.posts.joins(user: [group_users:[:group]]).where(groups: {name: params[:group]}).order(like_count: :desc).limit(1).first
+        if most_liked_post.id == post.id
+          render json: { highlight_post: true }
+        else
+          render json: { highlight_post: false }
+        end
       else
         render json: { highlight_post: false }
       end
